e942a09c8293069b966661bd15abef1183eece9b
El Basurero <elbasurerotivi@gmail.com>
2026-06-24 13:50:35 -0300

    fdsvs<

--- js/firebase.js
+++ js/firebase.js
@@ -1,21 +1,22 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
-
 import { getDatabase } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCaGUEDAS6Q_lRu3r1FZ-_NTF5hl1eoROw",
   authDomain: "goikorol.firebaseapp.com",
-  databaseURL: "https://goikorol-default-rtdb.firebaseio.com",
   projectId: "goikorol",
   storageBucket: "goikorol.firebasestorage.app",
   messagingSenderId: "524799531120",
-  appId: "1:524799531120:web:002688b10f2491f5ccbcb5",
-  measurementId: "G-RX6J13245F"
+  appId: "1:524799531120:web:002688b10f2491f5ccbcb5"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const db = getDatabase(app);
+
+// URL explícita
+export const db = getDatabase(
+  app,
+  "https://goikorol-default-rtdb.firebaseio.com"
+);
