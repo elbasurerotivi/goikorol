508a60ffe225c603759727526cfea0660e28d42d
El Basurero <elbasurerotivi@gmail.com>
2026-06-24 14:06:02 -0300

    fbae

--- js/auth.js
+++ js/auth.js
@@ -1,41 +1,31 @@
 import { auth } from "./firebase.js";
-
-import {
-    GoogleAuthProvider,
-    signInWithPopup,
-    signOut,
-    onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+import { GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
 const provider = new GoogleAuthProvider();
-
 window.loginGoogle = async () => {
-
     try {
-
         const result = await signInWithPopup(
             auth,
             provider
         );
-
         console.log("Login OK");
         console.log(result.user);
-
     } catch (err) {
-
         console.error(err);
-
     }
 };
 
 window.logoutUser = async () => {
-
     await signOut(auth);
-
 };
 
 onAuthStateChanged(auth, user => {
-
     console.log("Estado auth:", user);
 
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    document
+        .getElementById("login-btn")
+        ?.addEventListener("click", loginGoogle);
 });
