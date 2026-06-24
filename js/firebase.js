c6c921649ee77770ba6b43efbce825914844f3b4
El Basurero <elbasurerotivi@gmail.com>
2026-06-24 13:42:32 -0300

    grg

--- js/firebase.js
+++ js/firebase.js
@@ -1,5 +1,3 @@
-// js/firebase.js
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
