747c801a101d00d8f1f750319c13251c26e81bdd
El Basurero <elbasurerotivi@gmail.com>
2026-06-24 14:03:10 -0300

    wrvwe

--- js/firebase.js
+++ js/firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCaGUEDAS6Q_lRu3r1FZ-_NTF5hl1eoROw",
@@ -8,16 +7,9 @@ const firebaseConfig = {
   projectId: "goikorol",
   storageBucket: "goikorol.firebasestorage.app",
   messagingSenderId: "524799531120",
-  appId: "1:524799531120:web:002688b10f2491f5ccbcb5",
-  measurementId: "G-RX6J13245F"
+  appId: "1:524799531120:web:002688b10f2491f5ccbcb5"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-
-// URL explícita
-export const db = getDatabase(
-  app,
-  "https://goikorol-default-rtdb.firebaseio.com"
-);
